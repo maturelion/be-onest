661b8465e0f44563d04e0c599b1ecb5a4f14ffa3
explore <explore@Ganius-MacBook-Pro.local>
2023-10-06 00:22:17 +0100

    classified create

--- classifieds/views.py
+++ classifieds/views.py
@@ -1,8 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
-from .models import Classified
+from rest_framework import status
+from rest_framework.parsers import MultiPartParser, FormParser
+from .models import Classified, Photo
 from .serializers import ClassifiedSerializer
+from wallets.models import Wallet
+from django.db import transaction
+import environ
+
+env = environ.Env()
 
 
 class ClassifiedViewSet(ModelViewSet):
@@ -11,19 +18,29 @@ class ClassifiedViewSet(ModelViewSet):
     lookup_field = "id"
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ["country__name", "state__name", "city__name", "owner"]
+    parser_classes = (MultiPartParser, FormParser)
 
-    def perform_create(self, serializer):
-        classified = serializer.save()
+    def create(self, serializer):
+        user_wallet = Wallet.objects.get(user=self.request.user)
+        ads_overall_price = int(env("PRICE_PER_ADS"))
 
-        # Get the list of photo IDs from the request data
-        photo_ids = self.request.data.get('photos', [])
+        if user_wallet.balance >= ads_overall_price:
+            with transaction.atomic():
+                classified = serializer.save()
+                user_wallet.balance -= ads_overall_price
+                user_wallet.save()
+                # Get the list of photo from the request data
+                photo_ids = self.request.data.getlist('photos', [])
 
-        # Associate the selected photos with the ad
-        for photo_id in photo_ids:
-            try:
-                photo = Photo.objects.get(id=photo_id)
-                classified.photos.add(photo)
-            except Photo.DoesNotExist:
-                pass  # Handle the case where the photo doesn't exist
-
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+                # Associate the selected photos with the ad
+                for photo_id in photo_ids:
+                    try:
+                        photo = Photo.objects.create(
+                            image=photo_id, user=self.request.user)
+                        classified.photos.add(photo)
+                    except Photo.DoesNotExist:
+                        pass  # Handle the case where the photo doesn't exist
+                print(user_wallet, "balance after")
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"error": "Insufficient funds"}, status=status.HTTP_402_PAYMENT_REQUIRED)
